060d34786d345309988ed85cc01e1666d3edc0e7
Lndqw12 <COMP222012016@mail.ru>
2016-09-27 23:02:35 +0300

    27092016php

---
+++ new.php
@@ -0,0 +1,10 @@
+﻿<?php
+$files = array(
+	"img1,5.gif",
+	"img4.gif",
+	"img2,5.gif",
+	"img02.gif",
+	);
+ksort($files);
+echo "<pre>"; print_r($files); echo "</pre>";
+?>
---
+++ new1.php
@@ -0,0 +1,11 @@
+﻿<?php
+$numbers = array(
+	"a" => "zero",
+	"b" => "one",
+	"c" => "four",
+	"d" => "five",
+);
+sort($numbers);
+print_r($numbers);
+echo "</pre>";
+?>
